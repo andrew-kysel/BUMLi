3f19cd11d9b7ec2ab5ac3fd509c8cb505b511eeb
andrew-kysel <kyselov.andrii@gmail.com>
2025-08-26 13:37:02 +0300

    Init commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+bl_info = {
+  "name": "BUMLi",
+  "author": "Knight of Gamedevalry",
+  "version": (0, 1, 0),
+  "blender": (4, 3, 0),
+  "location": "3D Viewport > Sidebar > BUMLi",
+  "description": "Fixes meshes before exporting it to Unreal",
+  "category": "Development",
+}
+
